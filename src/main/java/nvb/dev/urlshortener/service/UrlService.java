76169ae49a69ef7b6a97037f949ed7c498436c41
erfannvb <erfannavab@gmail.com>
2026-08-04 13:14:59 +0330

    add: implemented a method to generate random short codes

--- src/main/java/nvb/dev/urlshortener/service/UrlService.java
+++ src/main/java/nvb/dev/urlshortener/service/UrlService.java
@@ -4,14 +4,34 @@ import nvb.dev.urlshortener.dto.CreateUrlRequest;
 import nvb.dev.urlshortener.dto.CreateUrlResponse;
 import org.springframework.stereotype.Service;
 
+import java.util.Random;
+
 @Service
 public class UrlService {
 
+    private static final Random RANDOM = new Random();
+    private static final int SHORT_CODE_LENGTH = 6;
+
+    private static final String CHARACTER_SET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
     public CreateUrlResponse shortenUrl(CreateUrlRequest request) {
-        if (!request.url().startsWith("http://") && !request.url().startsWith("https://"))
+        String url = request.url();
+
+        if (!url.startsWith("http://") && !url.startsWith("https://"))
             throw new IllegalArgumentException();
 
-        return new CreateUrlResponse(request.url());
+        return new CreateUrlResponse(generateShortCode());
+    }
+
+    private String generateShortCode() {
+        StringBuilder result = new StringBuilder(6);
+
+        for (int i = 0; i < SHORT_CODE_LENGTH; i++) {
+            int index = RANDOM.nextInt(CHARACTER_SET.length());
+            result.append(CHARACTER_SET.charAt(index));
+        }
+
+        return result.toString();
     }
 
 }
