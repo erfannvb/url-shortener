0ef4a620c04e0eff3825f75a12c159b5f8923cde
erfannvb <erfannavab@gmail.com>
2026-08-05 23:52:06 +0330

    add: implemented saving url into the database

--- src/main/java/nvb/dev/urlshortener/repository/UrlRepository.java
+++ src/main/java/nvb/dev/urlshortener/repository/UrlRepository.java
@@ -4,8 +4,12 @@ import nvb.dev.urlshortener.domain.ShortUrl;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UrlRepository extends JpaRepository<ShortUrl, UUID> {
+
+    Optional<ShortUrl> findByOriginalUrl(String originalUrl);
+
 }
--- src/main/java/nvb/dev/urlshortener/service/UrlService.java
+++ src/main/java/nvb/dev/urlshortener/service/UrlService.java
@@ -1,12 +1,17 @@
 package nvb.dev.urlshortener.service;
 
+import lombok.RequiredArgsConstructor;
+import nvb.dev.urlshortener.domain.ShortUrl;
 import nvb.dev.urlshortener.dto.CreateUrlRequest;
 import nvb.dev.urlshortener.dto.CreateUrlResponse;
+import nvb.dev.urlshortener.repository.UrlRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import java.util.Random;
 
 @Service
+@RequiredArgsConstructor
 public class UrlService {
 
     private static final Random RANDOM = new Random();
@@ -14,17 +19,32 @@ public class UrlService {
 
     private static final String CHARACTER_SET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+    private final UrlRepository urlRepository;
+
     public CreateUrlResponse shortenUrl(CreateUrlRequest request) {
         String url = request.url();
+        String normalizedUrl = url.toLowerCase();
 
-        if (!url.startsWith("http://") && !url.startsWith("https://"))
+        if (!normalizedUrl.startsWith("http://") && !normalizedUrl.startsWith("https://"))
             throw new IllegalArgumentException();
 
-        return new CreateUrlResponse(generateShortCode());
+        Optional<ShortUrl> existingShortUrl = urlRepository.findByOriginalUrl(url);
+        if (existingShortUrl.isPresent()) {
+            return mapShortUrlToResponse(existingShortUrl.get());
+        }
+
+        String shortCode = generateShortCode();
+        ShortUrl newShortUrl = ShortUrl.builder()
+                .originalUrl(url)
+                .shortCode(shortCode)
+                .build();
+
+        ShortUrl savedShortUrl = urlRepository.save(newShortUrl);
+        return mapShortUrlToResponse(savedShortUrl);
     }
 
     private String generateShortCode() {
-        StringBuilder result = new StringBuilder(6);
+        StringBuilder result = new StringBuilder(SHORT_CODE_LENGTH);
 
         for (int i = 0; i < SHORT_CODE_LENGTH; i++) {
             int index = RANDOM.nextInt(CHARACTER_SET.length());
@@ -32,6 +52,10 @@ public class UrlService {
         }
 
         return result.toString();
+    }
+
+    private CreateUrlResponse mapShortUrlToResponse(ShortUrl shortUrl) {
+        return new CreateUrlResponse(shortUrl.getShortCode());
     }
 
 }
